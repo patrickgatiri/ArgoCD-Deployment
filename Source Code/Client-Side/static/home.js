a9adc4d2eedfa605da69693bc304f8b8a659cab9
patrickgatiri <patrickgatiri@gmail.com>
2022-02-20 14:31:30 +0300

    Pointed frontend JS to Nodeport service that routes requests to the backend server

--- Source Code/Client-Side/static/home.js
+++ Source Code/Client-Side/static/home.js
@@ -19,8 +19,14 @@ var clicked = function(){
     };
 
     // Within the pod, the server listens on port 3000.
-    // However, the Nodeport routes requests on port 30000
+    // However, the Nodeport routes requests on port 30000.
+
+    //Implementation on baremetal server.
+    //serverRequest.open('GET', "http://localhost:3000/random", true);
+
+    //Implementation on minikube K8S cluster using nodeport.
     serverRequest.open('GET', "http://localhost:30000/random", true);
+
     serverRequest.send();
 }
 
